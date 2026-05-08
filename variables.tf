1b8ff804cd43d887921bfab43fbceb37b2f52d80
Mohammed Sulaiman <mohammedsulaimana9@gmail.com>
2026-05-09 02:48:54 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -21,4 +21,14 @@ variable "bucket_name" {
 variable "key_name" {
   type = string
 }
+variable "vault_addr" {
+  description = "Vault server address"
+  type        = string
+}
+
+variable "vault_token" {
+  description = "Vault authentication token"
+  type        = string
+  sensitive   = true
+}
 
